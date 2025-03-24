9f0b63d3c9caaa9f0fe88742fbf1477fb3be8abe
LJMSTSAstudent <162632476+LJMSTSAstudent@users.noreply.github.com>
2025-03-23 21:30:04 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,4 +12,34 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
-// Pushing This File
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+    0:{
+      slidesPerView: 1,
+    },
+    520:{
+      slidesPerView: 2,
+    },
+    950:{
+      slidesPerView: 3,
+    },
+  },
+});
